83fb42b13ed32a95ab109ec8aadf12dc1aa2e975
YeYunFeng <269581661@qq.com>
2019-01-25 18:13:42 +0800

    创建服务器监听，创建package.json,保存项目依赖

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+*小肥牛扫码点餐API子系统 
+*/
+const PORT = 8090;
+const express = require('express');
+
+//创建服务器
+var app = express();
+app.listen(PORT,()=>{
+  console.log('Server Listening'+POST+'...')
+});
